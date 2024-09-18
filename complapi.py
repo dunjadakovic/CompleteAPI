cc03d30d659e51147ab1f5fa4fe218752a4230bc
dunjadakovic <129779699+dunjadakovic@users.noreply.github.com>
2024-09-18 14:42:41 +0200

    Update complapi.py

--- complapi.py
+++ complapi.py
@@ -1,5 +1,4 @@
 from flask import Flask, request, jsonify, send_file
-from bark import SAMPLE_RATE, generate_audio, preload_models
 from scipy.io.wavfile import write as write_wav
 from IPython.display import Audio
 from flask_caching import Cache
